3b8c01298a3dd963894345a2fa9ae6dfc7bda410
Fran Tufro <hi@frantufro.com>
2023-08-24 17:32:53 -0300

    Scene Database command and github workflow

--- scene-finder/src/main.rs
+++ scene-finder/src/main.rs
@@ -1,23 +1,37 @@
+use catalyst::ContentId;
 
+use std::string::String;
+
+use serde_json::Value;
+use catalyst::Server;
+use core::time;
+use std::thread;
+
+use indicatif::ProgressBar;
 use std::fs::File;
 use chrono::{DateTime, Utc};
 use rss::{Channel, ChannelBuilder, Item, ItemBuilder};
-use dcl_common::Parcel;
+use dcl_common::{Parcel, Result};
 use std::env;
+use serde::Serialize;
 
 
-fn main() -> std::io::Result<()> {
+const MIN_PARCEL: i16 = -152;
+const MAX_PARCEL: i16 = 152;
+
+#[derive(Serialize)]
+struct ParcelPointer<'a> {
+    pointers: &'a Vec<Parcel>,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
   let args: Vec<String> = env::args().collect();
   
   let filename = args.get(1).unwrap();
+  
 
-  // TODO(fran): Update this with the actual list generation.
-  // let scenes = generate_list();
-  let scenes = vec![Scene {
-    title: "Test Scene".to_string(),
-    start_parcel: Parcel(10,10),
-    last_updated: Utc::now()
-  }];
+  let scenes = generate_list().await?;
 
   let channel = generate_rss(&scenes);
 
@@ -51,11 +65,73 @@ impl Scene {
   }
 }
 
+async fn generate_list() -> Result<Vec<Scene>> {
+  let server = Server::production();
+  let mut result = vec![];
 
-fn generate_list() -> Vec<Scene> {    
-    vec![]
+  let mut parcels = vec![];
+
+  for x in MIN_PARCEL..=MAX_PARCEL {
+    for y in MIN_PARCEL..=MAX_PARCEL {
+      parcels.push(Parcel(x, y));
+    }
+  }
+
+
+  let pointers = &parcels.to_vec();
+
+  let pointers = ParcelPointer {
+    pointers
+  };
+  
+  let scenes = server.raw_post("/content/entities/active", &pointers).await?;
+  let scenes = scenes.json().await?;
+  
+  if let Value::Array(scenes) = scenes {
+    let pb = ProgressBar::new(scenes.len().try_into().unwrap());
+    for scene in &scenes {
+      let scene = scene.as_object().unwrap();
+      if let Some(content) = scene.get("content") {
+        for object in content.as_array().unwrap() {
+          let object = object.as_object().unwrap();
+          if let Some(Value::String(file)) = object.get("file") {
+            if file.contains("2dcl/scene.2dcl") {
+              if let Some(hash) = object.get("hash") {
+                add_scene(hash.to_string().replace('"', ""), &server, &mut result).await?;
+              }
+            }
+          }
+        }
+        pb.inc(1);
+      }
+      thread::sleep(time::Duration::from_millis(1));
+    }
+  }
+  Ok(result)
 }
 
+async fn add_scene(content_id: String, server: &Server, results: &mut Vec<Scene>) -> Result<()> {
+  let url = format!("/content/contents/{}", content_id);
+  let response = server
+    .raw_get(url)
+    .await?;
+
+  let content = response.bytes().await?;
+
+  let downloaded_scene = dcl2d_ecs_v1::Scene::from_mp(&content.to_vec())?;
+
+  let scene = Scene {
+    last_updated: downloaded_scene.timestamp.into(),
+    title: downloaded_scene.name.clone(),
+    start_parcel: downloaded_scene.base.clone(),
+  };
+
+  println!("Adding Scene: ({:?}) {}", scene.start_parcel, scene.title);
+  results.push(scene);
+
+  Ok(())
+
+}
 const RSS_TITLE : &str = "2dcl Scenes";
 const RSS_LINK : &str = "https://2dcl.org/scenes";
 const RSS_DESCRIPTION : &str = 
